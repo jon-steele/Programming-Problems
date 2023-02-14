e12e0d42dc4d8d51f00ccdbe971ec84759687ca2
Jon Steele <jonwhsteele@gmail.com>
2023-02-14 14:03:46 -0800

    Completes OutOfPlace

--- OutOfPlace/main.cpp
+++ OutOfPlace/main.cpp
@@ -1,19 +1,12 @@
 //http://www.usaco.org/index.php?page=viewproblem2&cpid=785
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int heights[100];
-vector<int> seen;
+int sorted[100];
 int n;
-
-bool seenContains(int x){
-    for (int i = 0; i < seen.size(); i++) {
-        if (x == seen[i])
-            return true;
-    }
-    return false;
-}
 
 int main()
 {
@@ -23,42 +16,17 @@ int main()
     
     for (int i = 0; i < n; i++) {
         cin >> heights[i];
+        sorted[i] = heights[i];
     }
 
-    // Find Bessie
+    sort(sorted, sorted + n);
 
-    int bessie = 0;
-    int bessieIndex = 0;
+    int counter = 0;
 
-    if (n >= 3){
-        for (int i = 1; i < n; i++) {
-            if ((heights[i - 1] < heights[i] && heights[i] > heights[i + 1]) || (heights[i - 1] > heights[i] && heights[i] < heights[i + 1])) {
-                bessie = heights[i];
-                bessieIndex = i;
-                break;
-            }
-        }
-        // Find where Bessie belongs
-
-        int bessieBelongs = 0;
-
-        for (int i = 0; i < bessieIndex; i++) {
-            if (bessie < heights[i]){
-                bessieBelongs = i;
-                break;
-            }
-        }
-
-        // Count the number of unique elements from where bessie belongs, to where bessie is
-
-        for (int i = bessieBelongs; i < bessieIndex; i++) {
-            if (!seenContains(heights[i]))
-                seen.push_back(heights[i]);
-        }
-
-        cout << seen.size() << endl;
+    for (int i = 0; i < n; i++){
+        if (sorted[i] != heights[i])
+            counter++;
     }
-    else{
-        cout << 1 << endl;
-    }
+
+    cout << counter - 1 << endl;
 }
