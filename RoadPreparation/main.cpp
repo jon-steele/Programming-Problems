658e793eee15758e71e38c094ff5937f3dc62407
Jon Steele <mrjonsteele@gmail.com>
2023-04-04 12:06:07 -0700

    Completed Road Preparation

---
+++ RoadPreparation/main.cpp
@@ -0,0 +1,71 @@
+// https://cses.fi/problemset/task/1675/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+int ds[100005]; // disjoint set, used to track groups of roads
+long long sum;
+pair<int, pair<int, int> > roads[200005];
+
+// Search returns the root of the group of a node
+int search(int node){
+    
+    if(ds[node] < 0)
+       return node;
+    
+    ds[node] = search(ds[node]);
+    
+    return ds[node];
+}
+
+// Merge tries to merge two groups
+bool merge(int node1, int node2){
+    
+    node1 = search(node1); 
+    node2 = search(node2);
+    
+    if(node1 == node2)  
+        return false;
+    if(ds[node1] < ds[node2])   
+        swap(node1, node2);
+    
+    ds[node2] += ds[node1];
+    ds[node1] = node2;
+    
+    return true;
+}
+
+int main(){
+    cin >> n >> m;
+    int a, b, c;
+ 
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b >> c;
+        roads[i] = (make_pair(c, make_pair(a, b)));
+    }
+ 
+    sort(roads, roads + m);
+    fill(ds+1, ds+n+1, -1);
+
+    for(int i = 0; i < m; i++){
+
+        pair<int, pair<int, int> > edge = roads[i];
+        
+        // If we successfully merge the two groups, we can add their sum to our total
+        if (merge(edge.second.first, edge.second.second)){
+            
+            sum += edge.first;
+            n--;
+        }
+    }
+
+    if (n == 1)
+        cout << sum << endl;
+    else
+        cout << "IMPOSSIBLE" << endl;
+}
